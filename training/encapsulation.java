ea1a4974e4a0b9287da5d120bd3841b725245125
phenix-sin <prashu.51@gmail.com>
2015-07-26 13:53:50 +0530

    training
    for trainng

---
+++ training/encapsulation.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package training;
+
+/**
+ * @author prashant
+ *
+ */
+public class encapsulation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Badooo b = new Badooo();
+		b.size = 7;   //legal but bad 
+
+	}
+
+}
+ class Badooo {
+	public int size ;
+	public int height;
+	
+}
+
+
